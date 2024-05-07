1b4c2e112392e33f0cd84f02d236d215690a6768
thieu879 <nguyenthieu11021995@gmail.com>
2024-05-07 08:58:18 +0700

    addSs29

---
+++ ss29/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Bt1 from './conponent/bt1/Bt1'
+import Bt2 from './conponent/bt2/Bt2'
+import Bt3 from './conponent/bt3/Bt3'
+import Bt4 from './conponent/bt4/Bt4'
+import Bt5 from './conponent/bt5/Bt5'
+import Bt6 from './conponent/bt6/Bt6'
+import Bt7 from './conponent/bt7/Bt7'
+import Bt8 from './conponent/bt8/Bt8'
+import Bt9 from './conponent/bt9/Bt9'
+import Bt10 from './conponent/bt10/Bt10'
+export default function App() {
+  return (
+    <div>
+      <Bt1></Bt1>
+      <br />
+      <Bt2></Bt2>
+      <br />
+      <Bt3></Bt3>
+      <br />
+      <Bt4></Bt4>
+      <br />
+      <Bt5></Bt5>
+      <br />
+      <Bt6></Bt6>
+      <br />
+      <Bt7></Bt7>
+      <br />
+      <Bt8></Bt8>
+      <br />
+      <Bt9></Bt9>
+      <br />
+      <Bt10></Bt10>
+    </div>
+  )
+}
